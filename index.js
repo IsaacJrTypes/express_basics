e8b14c2518b03a49eec08cd5e398f93ffdcd2a82
Isaac J <duckhunterjr@gmail.com>
2024-01-25 15:44:11 -0800

    add conditional function rerouting

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.set('port',port);
+// Create random num generator btw 0 and 1
+function numGenerator() {
+    const range = 2;
+    const num = Math.floor(Math.random() * range); // Will only output 0 or 1
+    console.log(num);
+    return num;
+};
 
-app.get('/', (req,res) => {
-    res.type("text/plain");
-    res.send("Hello Mundo");
-})
+app.set('port', port);
 
-app.get('/about', (req, res) => {
-    res.type("text/plain");
-    res.send("About Page");
+app.get('/', (req, res) => {
+    res.type('text/plain');
+    res.send('Hello Mundo');
 });
 
-app.use((req,res) => {
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send('About Page');
+});
+
+
+app.get('/foo', (req, res, next) => {
+    const num = numGenerator();
+    if (num === 0) {
+        res.type('text/plain');
+        res.send('sometimes this');
+    } else {
+        next();
+    }
+});
+
+app.get('/foo', (req, res) => {
+    res.type('text/plain');
+    res.send('and sometimes that');
+});
+
+app.use((req, res) => {
     res.type('text/plain');
     res.status(404);
-    res.send("404: Not Found")
-})
+    res.send('404: Not Found');
+});
 
 app.listen(app.get('port'), () => {
-    console.log("Express running at " + port)
-    console.log("In browser go to: http://localhost:3000/")
-})
+    console.log(`Express running at ${port}`);
+    console.log(`In browser go to: http://localhost:${port}`);
+});
